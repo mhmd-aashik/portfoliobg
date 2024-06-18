fb7979fe45a04623376c5b9318f708bfff9502a7
mhmd-aashik <aashikdevelop@gmail.com>
2024-06-18 10:52:33 +0530

    error1

--- components/shared/blogs/BlogsItem.tsx
+++ components/shared/blogs/BlogsItem.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import BlogCards from "../BlogCards";
-import { useParams, useSearchParams } from "next/navigation";
 import Link from "next/link";
 
 const BlogsItem = ({ blogs }: any) => {
